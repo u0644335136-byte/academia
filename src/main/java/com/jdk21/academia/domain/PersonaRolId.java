9d056d274b260e878486df61aa4f66f949e7afa9
Diego Guaman <diegoguamanmedina@gmail.com>
2025-11-03 16:47:19 +0100

    - Se introdujo SecurityConfig para quitar la seguridad de la API, por ahora
    - Se añadió PersonaController para gestionar personas con operaciones CRUD.
    
    - Se implementaron DTO para la creación, actualización y asignación de roles de personas.
    
    - Se creó PersonaService para la lógica de negocio relacionada con las personas.
    
    - Se integró MapStruct para el mapeo de entidades a DTO.
    
    - Se actualizó pom.xml para incluir la dependencia de validación de Spring Boot y se ajustaron los ajustes del plugin del compilador.
    
    - Se mejoró PersonaRolId con soporte para serialización y se añadieron métodos al repositorio de roles.

--- src/main/java/com/jdk21/academia/domain/PersonaRolId.java
+++ src/main/java/com/jdk21/academia/domain/PersonaRolId.java
@@ -1,14 +1,22 @@
 package com.jdk21.academia.domain;
+
 import java.io.Serializable;
 
 import lombok.*;
 
+/**
+ * Clase que representa la clave primaria compuesta para la entidad PersonaRol.
+ * Debe implementar Serializable y tener equals() y hashCode() para funcionar correctamente con JPA.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class PersonaRolId implements Serializable{
+public class PersonaRolId implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
     private Long idPersona;
     private Long idRol;
 }
--- src/main/java/com/jdk21/academia/features/persona/repository/PersonaRolRepository.java
+++ src/main/java/com/jdk21/academia/features/persona/repository/PersonaRolRepository.java
@@ -2,8 +2,9 @@ package com.jdk21.academia.features.persona.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.jdk21.academia.domain.PersonaRol;
 import com.jdk21.academia.domain.PersonaRolId;
 
-public interface PersonaRolRepository extends JpaRepository<PersonaRolId, Long> {
+public interface PersonaRolRepository extends JpaRepository<PersonaRol, PersonaRolId> {
 
 }
